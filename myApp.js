f426c986b98e94d7d017a1d3511442449a0416e2
youssefziat02 <youssefziat02@gmail.com>
2020-08-08 10:33:35 +0100

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -21,10 +21,15 @@ app.get("/", function(req, res) {
   });
 
 /** 4) Serve static assets  */
+app.use(express.static(__dirname + "/public"));
 
 
 /** 5) serve JSON on a specific route */
-
+app.get("/json", (req,res) => {
+    res.json({
+        "message": "Hello json"
+    })
+})
 
 /** 6) Use the .env file to configure the app */
  
